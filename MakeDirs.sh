cb7e580e04abae239be8d7b1d6e678985721fb7b
Rich Jackson <Richard.Jackson@walmart.com>
2017-03-03 18:08:57 -0600

    Sync with PDS structure

--- MakeDirs.sh
+++ MakeDirs.sh
@@ -5,7 +5,7 @@ mkdir ./asm 2> /dev/null
 mkdir ./cbl 2> /dev/null
 mkdir ./cpy 2> /dev/null
 mkdir ./exec 2> /dev/null
-mkdir ./idcams 2> /dev/null                                             null
+mkdir ./idcams 2> /dev/null
 mkdir ./jcl 2> /dev/null
 mkdir ./mac 2> /dev/null
 mkdir ./rdo 2> /dev/null
